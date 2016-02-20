f88a54d727c6ff2371a3485df5f2ed225f79820f
hiteshbargujar <hitesh.bargujar@hobsons.com>
2016-02-20 08:37:17 -0500

    no message

---
+++ src/main/java/org/javainterviewprep/examples/finallyBlock/ShopKeeper.java
@@ -0,0 +1,18 @@
+package org.javainterviewprep.examples.finallyBlock;
+
+/*
+ * Q1. What is Finally Block ?
+ * - Finally block is a piece of Code which gets executed ALWAYS, 
+ *   during normal flow of the program or during exception also.
+ * 
+ * - Finally block is used after TRY-CATCH block.
+ * 
+ * - Generally code to close connection or stream are executed as part of Finally Block.
+ */
+
+//Shopkeeper has a mechanism to check customer membership.
+//Shopkeeper provides form for membership if not a member already.
+//Shopkeeper provides all customers greeting cards for "Happy Shopping".
+public class ShopKeeper {
+
+}
